60072b1ba99a49a0dc80a2adc2ab58fb6679856b
kyominkim1074 <kyominkim1074@gmail.com>
2022-10-14 14:58:08 +0900

    reviews/index

--- reviews/views.py
+++ reviews/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, redirect
+
+from reviews.models import Review
 from .forms import ReviewForm
 
 # Create your views here.
@@ -7,10 +9,17 @@ def create(request):
         form = ReviewForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('accounts:index')
+            return redirect('reviews:index')
     else:
         form = ReviewForm()
     context = {
         'form' : form
     }
     return render(request, 'reviews/create.html', context)
+
+def index(request):
+    review = Review.objects.order_by('-pk')
+    context = {
+        'review': review
+    }
+    return render(request, 'reviews/index.html', context)
