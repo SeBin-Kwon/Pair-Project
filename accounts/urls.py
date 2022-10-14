2641ee9f4732c618ef2da2f874c9347357021c15
sebin kwon <sbkwon98@gmail.com>
2022-10-14 11:14:04 +0900

    index.html

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('', views.main, name='main'),
     path('accounts/signup', views.signup, name='signup'),
     path('accounts/login', views.login, name='login'),
+    path('accounts/', views.index, name='index'),
 ]
